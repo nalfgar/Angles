54b25cce9efe7f8b37af9b430d89dc1a5ffbd7f3
darek <dstroj@wp.pl>
2019-12-06 23:42:14 +0100

    Add Angle.class.

---
+++ src/main/java/pl/strojecki/Angle.java
@@ -0,0 +1,37 @@
+package pl.strojecki;
+
+import lombok.ToString;
+
+import static java.lang.Double.parseDouble;
+
+@ToString
+public class Angle {
+
+    @ToString.Exclude
+    private String inputString;
+    private double value;
+    private final AngleType angleType;
+
+    public Angle(String inputString, AngleType angleType) {
+        this.inputString = inputString;
+        this.angleType = angleType;
+
+        if (angleType == AngleType.GRAD) {
+            inputString = inputString.replace(',','.');
+            value = parseDouble(inputString);
+        }
+        value = normalizeAngle(value, this.angleType);
+    }
+
+    private double normalizeAngle(double value, AngleType angleType) {
+        while (value >= angleType.getMaxValue()) {
+            value -= angleType.getMaxValue();
+        }
+        while (value < 0.0) {
+            value += angleType.getMaxValue();
+        }
+
+        return value;
+    }
+
+}
--- src/main/java/pl/strojecki/Angles.java
+++ src/main/java/pl/strojecki/Angles.java
@@ -3,6 +3,7 @@ package pl.strojecki;
 public class Angles {
 
     public static void main(String[] args) {
-        System.out.println(AngleType.DD);
+        Angle angle = new Angle("405.01", AngleType.GRAD);
+        System.out.println(angle);
     }
 }
